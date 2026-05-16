0bf2b5dd37d86e760aac21099cbdf8da1563660d
Tochukwu Nwachukwu <tochukwunwachukwu619@gmail.com>
2026-05-16 19:46:20 +0100

    Assignment of day 2

---
+++ variables.py
@@ -0,0 +1,41 @@
+# Day 2: 30 Days of python programming
+#level 1
+first_name = "Tochukwu"
+last_name = "Nwachukwu"
+full_name = "Tochukwu Nwachukwu"
+country = "Nigeria"
+city = "Abuja"
+age = 18
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+first, middle, last, current_age = "Tochukwu", "Gideon", "Nwachukwu", 18
+print(middle)
+
+#level 2
+print(type(full_name), type(country), type(city), type(age), type(is_married), type(is_true),type(is_light_on))
+print (len(first_name))
+compare = (len(first_name) == len(last_name))
+print(compare)
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+circum_of_circle = 2 * 3.14 * radius
+radius = int(input("Enter the raidus: "))
+area = 3.14 * radius ** 2
+print (area)
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+country = input("Enter your country: ")
+age = int(input("Enter your age: "))
+
+help('keywords')
