dbeab16d79d25c97c7d70cafbe9fc4a03efe65a8
Tochukwu Nwachukwu <tochukwunwachukwu619@gmail.com>
2026-05-26 23:44:25 +0100

    exercises

---
+++ tochukwus python docs/exercises week 1/day_1.py
@@ -0,0 +1,32 @@
+# Integer
+age = 25
+
+# Float
+height = 1.78
+
+# Complex
+complex_num = 2 + 3j
+
+# String
+name = "Tochukwu"
+
+# Boolean
+is_student = True
+
+# List
+fruits = ["mango", "banana", "orange"]
+
+# Tuple
+coordinates = (2, 3)
+
+# Set
+unique_numbers = {1, 2, 3, 4, 5}
+
+# Dictionary
+person = {"name": "Tochukwu", "age": 18, "city": "Abuja"}
+
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+distance = ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
+print(f"Euclidean distance: {distance}") 
